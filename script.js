3ec3a9b9af5218bc8068d2b77e42fee1c600041c
gob77 <mraortiz@hotmail.com>
2017-03-22 13:09:04 -0300

    added style, added changin color function, still need work

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// this was originally made in sublime text 3 then uploaded to codepen,
+// it was tested in a local host made it with npm http-server and using google chrome,
+// still need some work in the quote's font
+
 var tweet;
+var tweetUrl = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=";
 
 function getQuote() {
   var quote = document.getElementById("quote");
@@ -10,18 +15,50 @@ function getQuote() {
       data = xhr.responseText;
       quote.innerHTML = JSON.parse(data)[0].content;
       author.innerHTML = JSON.parse(data)[0].title;
-      tweet = JSON.parse(data)[0].content.replace("<p>", "").replace("</p>","") + " - " + JSON.parse(data)[0].title;
-
+      tweet = JSON.parse(data)[0].content.replace("<p>", "'").replace("</p>", "'") + " - " + JSON.parse(data)[0].title;
+    }
+    var tweetBTN = document.getElementById("tweetQuote");
+    if(JSON.parse(data)[0].content.length + JSON.parse(data)[0].title.length > 140) {
+      tweetBTN.disabled = true;
+    }
+    else {
+     tweetBTN.disabled = false;
     }
   }
   xhr.open("GET", "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&cache="+Date.now(), true);
   xhr.send();
 }
 
-var btn = document.getElementById("btn");
+var btn = document.getElementById("newQuote");
 btn.addEventListener("click", getQuote);
 
+function tweetQuote(){
+  window.location = encodeURI(tweetUrl +  "'" + tweet + "'") + "_blank";
+}
 
-var tweetUrl = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=";
+var tweetBTN = document.getElementById("tweetQuote");
+tweetBTN.addEventListener("click", tweetQuote);
 
-// encondeUrl(tweetUrl +  "'" + tweet + "'")
+// The change color function is not finished, there are colors too bright and it will be difficult to read the quotes
+
+function changeColor() {
+
+  var r, g, b;
+
+  r = Math.floor(Math.random()*255);
+  g = Math.floor(Math.random()*255);
+  b = Math.floor(Math.random()*255);
+
+  var gradient = document.getElementById("gradient");
+  var header = document.getElementById("main-header");
+  var quote = document.getElementById("quote");
+  var author = document.getElementById("author");
+
+  gradient.style.backgroundColor = "rgb(" + r + "," + g + "," + b +")";
+  quote.style.color = "rgb(" + r + "," + g + "," + b +")";
+  author.style.color = "rgb(" + r + "," + g + "," + b +")";
+  header.style.backgroundColor = "rgb(" + Math.floor(r*0.80) + "," + Math.floor(g*0.80) + "," + Math.floor(b*0.80) +")";
+}
+
+var btn = document.getElementById("newQuote");
+btn.addEventListener("click", changeColor);
